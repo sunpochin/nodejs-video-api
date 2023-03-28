7d7fb3578f37503e4415d49ad3e69ab3a6618689
pac <sunpochin@gmail.com>
2023-03-29 04:51:48 +0800

    Revert "adding google.post.js"
    This reverts commit 53f078ff89fb28ab8a53ff0058db519b255a004d.

--- src/index.ts
+++ src/index.ts
@@ -3,82 +3,45 @@
 
 import * as dotenv from 'dotenv';
 import express from 'express';
-// import helmet from 'helmet';
+import helmet from 'helmet';
 import { itemsRouter } from './router/items.router';
-import session from "express-session";
-import auth from "./router/auth.router";
+import router from "./router/auth.router";
 import cors from 'cors';
 var cookies = require('cookie-parser');
-const passport = require('passport');
-const cookieSession = require('cookie-session');
-require('./passport');
+// const session = require('express-session');
+// const passport = require('passport');
+
+// // 設定 Passport 身分驗證套件
+// passport.serializeUser(function (user, done) {
+// 	done(null, user);
+// });
+
+// passport.deserializeUser(function (user, done) {
+// 	done(null, user);
+// });
 
 dotenv.config();
 
 const app = express();
 // https://juejin.cn/post/7117208351234064414
 // use cors has to be before use router?
-export const UI_ROOT_URI = 'http://localhost:5173';
-//app.use(cookies());
-// app.use(
-// 	cookieSession({
-// 		name: 'google-auth-session',
-// 		keys: ['key1', 'key2'],
-// 	})
-// );
-// Authentication configuration
-app.use(session({
-  resave: false,
-  saveUninitialized: true,
-  secret: 'bla bla bla'
-}));
+export const UI_ROOT_URI = 'http://localhost:8080';
 
-app.use(passport.initialize());
-app.use(passport.session());
-
+app.use(cookies());
 app.use(
 	cors({
 		// Sets Access-Control-Allow-Origin to the UI URI
-		origin: [
-			UI_ROOT_URI,
-			'https://localhost:5173',
-			'https://localhost:8000',
-			'http://localhost:8000',
-		],
+		origin: UI_ROOT_URI,
 		// Sets Access-Control-Allow-Credentials to true
 		credentials: true,
 	})
 );
 
-// app.use(helmet());
+
+app.use(helmet());
 app.use(express.json());
 app.use('/items', itemsRouter);
-app.use('/', auth);
-
-// app.get('/failed', (req, res) => {
-// 	res.send('Failed');
-// });
-// app.get('/success', (req, res) => {
-// 		console.log('req.user: ', req.user);
-// 	res.send(`Welcome ${req.user.displayName}`);
-// });
-
-// app.get(
-// 	'/google',
-// 	passport.authenticate('google', {
-// 		scope: ['email', 'profile'],
-// 	})
-// );
-
-// app.get(
-// 	'/auth/google/callback',
-// 	passport.authenticate('google', {
-// 		failureRedirect: '/failed',
-// 	}),
-// 	function (req, res) {
-// 		res.redirect('/success');
-// 	}
-// );
+app.use('/', router);
 
 // app.get('/', function (req, res) {
 // 	res.json('nodejs video');
@@ -89,6 +52,23 @@ let todos: string[] = ['todo 1', 'todo 2'];
 app.get('/todos', (req, res) => {
 	res.json(todos);
 });
+// app.post('/add', (req, res) => {
+// 	console.log('req.body: ', req.body);
+// 	res.json(req.body);
+// });
+
+// // const express = require('express');
+// const { OAuth2Client } = require('google-auth-library');
+// const { google } = require('googleapis');
+
+// const CLIENT_ID =
+// 	'266847528060-86qmub9fgtbtavdkvicc98h5qmot8iub.apps.googleusercontent.com';
+// const CLIENT_SECRET = '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';
+// const REDIRECT_URI = 'http://localhost:8000/auth/google/callback';
+// const COOKIE_NAME = 'cookie';
+// const JWT_SECRET = 'secret';
+// const oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+
 
 // 啟動服務器
 const port = 8000;
--- src/router/auth.router.ts
+++ src/router/auth.router.ts
@@ -1,9 +1,7 @@
 import express, { Request, Response } from 'express';
-const passport = require('passport');
 import jwt from 'jsonwebtoken';
 import querystring from 'querystring';
 import axios from 'axios';
-const { OAuth2Client } = require('google-auth-library');
 
 // const passport = require('passport');
 // const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
@@ -16,82 +14,94 @@ import {
 	GOOGLE_CLIENT_SECRET,
 	COOKIE_NAME,
 	UI_ROOT_URI,
-} from './config';
+} from "./config";
 
 const port = 8000;
 
 const redirectURI = 'auth/google/callback';
 
-// Getting the user from Google with the code
-router.get('/failed', (req, res) => {
-	res.send('Failed');
-});
 
-router.get('/success', (req, res) => {
-	console.log('req.user: ', req.user);
-	console.log('req.user: ', req.user.emails[0]);
-	res.send(`Welcome ${req.user.emails[0].value}`);
-});
+function getTokens({
+	code,
+	clientId,
+	clientSecret,
+	redirectUri,
+}: {
+	code: string;
+	clientId: string;
+	clientSecret: string;
+	redirectUri: string;
+}): Promise<{
+	access_token: string;
+	expires_in: Number;
+	refresh_token: string;
+	scope: string;
+	id_token: string;
+}> {
+	/*
+	 * Uses the code to get tokens
+	 * that can be used to fetch the user's profile
+	 */
+	console.log('get tokens');
+	const url = 'https://oauth2.googleapis.com/token';
+	const values = {
+		code,
+		client_id: clientId,
+		client_secret: clientSecret,
+		redirect_uri: redirectUri,
+		grant_type: 'authorization_code',
+	};
 
-
-router.post('/auth/google',async (req: Request, res: Response) => {
-	try {
-		const item = req.body;
-		console.log('req.body: ', req.body)
-		const access_token = req.body.access_token;
-    console.log('req.body accessToken: ', access_token);
-
-		const oauth2Client = new OAuth2Client()
-	  oauth2Client.setCredentials({ access_token: access_token });
-		const userInfo = await oauth2Client
-	    .request({
-	      url: 'https://www.googleapis.com/oauth2/v3/userinfo'
-	    })
-	    .then((response) => response.data)
-	    .catch(() => null)
-
-		console.log('userInfo: ', userInfo)
-
-	  // oauth2Client.revokeCredentials()
-
-		res.status(201).json(userInfo);
-	} catch (e: any) {
-		res.status(500).send(e.message);
-	}
-});
-
-
-// passport.authenticate('google', {
-// 	scope: ['email', 'profile'],
-// })
-router.get(
-	'/google',
-	passport.authenticate('google', {
-		scope: ['email', 'profile'],
-	})
-);
-
-router.get(
-	'/auth/google/callback',
-	passport.authenticate('google', {
-		failureRedirect: '/failed',
-	}),
-	function (req: Request, res: Response) {
-		res.data = req.user;
-		const token = jwt.sign(req.user, JWT_SECRET);
-		console.log('googleUser: ', req.user);
-		console.log('token: ', token);
-
-		res.cookie(COOKIE_NAME, token, {
-			maxAge: 900000,
-			httpOnly: false,
-			secure: false,
+	return axios
+		.post(url, querystring.stringify(values), {
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+		})
+		.then((res) => res.data)
+		.catch((error) => {
+			console.error(`Failed to fetch auth tokens`);
+			throw new Error(error.message);
 		});
+}
 
-		res.redirect('http://localhost:8080');
-		// res.redirect('/success');
-	}
-);
+// Getting the user from Google with the code
+router.get(`/${redirectURI}`, async (req, res) => {
+  const code = req.query.code as string;
+
+  const { id_token, access_token } = await getTokens({
+    code,
+    clientId: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    redirectUri: `${SERVER_ROOT_URI}/${redirectURI}`,
+  });
+
+  // Fetch the user's profile with the access token and bearer
+  const googleUser = await axios
+    .get(
+      `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${access_token}`,
+      {
+        headers: {
+          Authorization: `Bearer ${id_token}`,
+        },
+      }
+    )
+    .then((res) => res.data)
+    .catch((error) => {
+      console.error(`Failed to fetch user`);
+      throw new Error(error.message);
+    });
+
+  const token = jwt.sign(googleUser, JWT_SECRET);
+  console.log('googleUser: ', googleUser);
+  res.cookie(COOKIE_NAME, token, {
+    maxAge: 900000,
+    httpOnly: true,
+    secure: false,
+  });
+
+  res.redirect(UI_ROOT_URI);
+});
 
 // Getting the current user
 router.get("/auth/me", (req, res) => {
@@ -105,5 +115,6 @@ router.get("/auth/me", (req, res) => {
     res.send(null);
   }
 });
+
 
 module.exports = router;
