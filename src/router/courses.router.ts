230788aa89b9335eb18f6164efe7e2c37c7872ef
pac <sunpochin@gmail.com>
2023-03-30 03:48:14 +0800

    adding course route

--- src/router/courses.router.ts
+++ src/router/courses.router.ts
@@ -11,6 +11,9 @@ router.get('/', async (req, res) => {
   try {
 		let ret = auth.vali(req, res);
     console.log('ret: ', ret)
+    if (null == ret) {
+      res.send("請先登入！")
+    }
 		// res.send(ret);
 		res.send(ret.email + ' 的課程列表：貓咪餵食、貓砂選擇、貓咪玩耍')
 	} catch(err) {
