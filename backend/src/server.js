370e817387561bdad77d7ffd0e4b3c077ac9f8a4
pac <sunpochin@gmail.com>
2023-04-08 23:12:52 +0800

    adding server.js

---
+++ backend/src/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const youtube = require('youtube-api');
+
+const uuid = require('uuid');
+
+const cors = require('cors');
+
+// const open = require('open');
+
+const multer = require('multer');
+
+const credentials = require('./credentials.json');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+const PORT = 3000 | process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log('app listening on port ' + PORT);
+})
+
+
+
--- backend/src/youtube-upload.js
+++ backend/src/youtube-upload.js
@@ -1,177 +1,51 @@
-// YouTube API video uploader using JavaScript/Node.js
-// You can find the full visual guide at: https://www.youtube.com/watch?v=gncPwSEzq1s
-// You can find the brief written guide at: https://quanticdev.com/articles/automating-my-youtube-uploads-using-nodejs
-//
-// Upload code is adapted from: https://developers.google.com/youtube/v3/quickstart/nodejs
-
-const fs = require('fs');
-const readline = require('readline');
-const assert = require('assert');
-const { google } = require('googleapis');
-const OAuth2 = google.auth.OAuth2;
-
-// video category IDs for YouTube:
-const categoryIds = {
-	Entertainment: 24,
-	Education: 27,
-	ScienceTechnology: 28,
+// const upload = require('./youtube-upload.js')
+const { upload } = require('youtube-videos-uploader'); //vanilla javascript
+const credentials = {
+	email: 'pachinkosun@gmail.com',
+	pass: 'Qwer4455.',
+	recoveryemail: 'sunpochin@gmail.com',
 };
 
-// If modifying these scopes, delete your previously saved credentials in client_oauth_token.json
-const SCOPES = ['https://www.googleapis.com/auth/youtube.upload'];
-const TOKEN_PATH = '../' + 'client_oauth_token.json';
-
-const videoFilePath = '../vid.mp4';
-const thumbFilePath = '../thumb.png';
-
-exports.uploadVideo = (title, description, tags) => {
-	console.log('uploadVideo')
-	assert(fs.existsSync(videoFilePath));
-	assert(fs.existsSync(thumbFilePath));
-
-	// Load client secrets from a local file.
-	fs.readFile(
-		'../client_secret.json',
-		function processClientSecrets(err, content) {
-			if (err) {
-				console.log('Error loading client secret file: ' + err);
-				return;
-			}
-			// Authorize a client with the loaded credentials, then call the YouTube API.
-			authorize(JSON.parse(content), (auth) =>
-				uploadVideo(auth, title, description, tags)
-			);
-		}
-	);
+const onVideoUploadSuccess = (videoUrl) => {
+	// ..do something..
+	console.log('onVideoUploadSuccess!!');
 };
 
-/**
- * Upload the video file.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-function uploadVideo(auth, title, description, tags) {
-	console.log('uploadVideo 2');
-	const service = google.youtube('v3');
+const video1 = {
+	path: './vid.mp4',
+	title: 'title 1',
+	description: 'description 1',
+	onProgress: (progress) => {
+		console.log('percent: ', progress);
+	},
+	onSuccess: onVideoUploadSuccess,
+};
 
-	service.videos.insert(
-		{
-			auth: auth,
-			part: 'snippet,status',
-			requestBody: {
-				snippet: {
-					title,
-					description,
-					tags,
-					categoryId: categoryIds.ScienceTechnology,
-					defaultLanguage: 'en',
-					defaultAudioLanguage: 'en',
-				},
-				status: {
-					privacyStatus: 'private',
-				},
-			},
-			media: {
-				body: fs.createReadStream(videoFilePath),
-			},
-		},
-		function (err, response) {
-			if (err) {
-				console.log('The API returned an error: ' + err);
-				return;
-			}
-			console.log(response.data);
+const video2 = {
+	path: './Hello.mp4',
+	title: 'title 2',
+	description: 'description 2',
+	thumbnail: 'thumbnail.png',
+	language: 'english',
+	tags: ['video', 'github'],
+	playlist: 'playlist name',
+	channelName: 'Channel Name',
+	onSuccess: onVideoUploadSuccess,
+	skipProcessingWait: true,
+	onProgress: (progress) => {
+		console.log('progress', progress);
+	},
+	uploadAsDraft: false,
+	isAgeRestriction: false,
+	isNotForKid: false,
+	publishType: 'PUBLIC',
+	isChannelMonetized: false,
+};
 
-			console.log('Video uploaded. Uploading the thumbnail now.');
-			service.thumbnails.set(
-				{
-					auth: auth,
-					videoId: response.data.id,
-					media: {
-						body: fs.createReadStream(thumbFilePath),
-					},
-				},
-				function (err, response) {
-					if (err) {
-						console.log('The API returned an error: ' + err);
-						return;
-					}
-					console.log(response.data);
-				}
-			);
-		}
-	);
+function main() {
+  console.log('main')
+  // upload.uploadVideo('title', 'description', ['tag1', 'tag2'])
+  upload(credentials, [video1, video2]).then(console.log);
 }
 
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- *
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-	console.log('credentials: ', credentials);
-
-	const clientSecret = credentials.web.client_secret;
-	const clientId = credentials.web.client_id;
-	const redirectUrl = credentials.web.redirect_uris[0];
-	const oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
-
-	// Check if we have previously stored a token.
-	fs.readFile(TOKEN_PATH, function (err, token) {
-		if (err) {
-			getNewToken(oauth2Client, callback);
-		} else {
-			oauth2Client.credentials = JSON.parse(token);
-			callback(oauth2Client);
-		}
-	});
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- *
- * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback to call with the authorized
- *     client.
- */
-function getNewToken(oauth2Client, callback) {
-	const authUrl = oauth2Client.generateAuthUrl({
-		access_type: 'offline',
-		scope: SCOPES,
-	});
-	console.log('Authorize this app by visiting this url: ', authUrl);
-	const rl = readline.createInterface({
-		input: process.stdin,
-		output: process.stdout,
-	});
-	rl.question('Enter the code from that page here: ', function (code) {
-		rl.close();
-		oauth2Client.getToken(code, function (err, token) {
-			if (err) {
-				console.log('Error while trying to retrieve access token', err);
-				return;
-			}
-			oauth2Client.credentials = token;
-			storeToken(token);
-			callback(oauth2Client);
-		});
-	});
-}
-
-/**
- * Store token to disk be used in later program executions.
- *
- * @param {Object} token The token to store to disk.
- */
-function storeToken(token) {
-	fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-		if (err) throw err;
-		console.log('Token stored to ' + TOKEN_PATH);
-	});
-}
-
-
-uploadVideo();
+main();
