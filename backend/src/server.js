dbdb2828242ada6cabb99a9bc7c9b9ff706eac9d
pac <sunpochin@gmail.com>
2023-04-09 20:13:59 +0800

    open: ^8.1.0,

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,11 +6,12 @@ const uuid = require('uuid');
 
 const cors = require('cors');
 
-// const open = require('open');
+const open = require('open');
+// import open, { openApp, apps } from 'open';
 
 const multer = require('multer');
 
-const credentials = require('./credentials.json');
+// const credentials = require('./credentials.json');
 
 const app = express();
 
